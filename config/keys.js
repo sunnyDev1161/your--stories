90f260cf7c165861656ca364af68c8e17ea63e94
hasnainShahBukhari <hasnain.shah@globewyze.com>
2019-05-29 00:26:34 +0500

    handled google authetication

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   clientID:
-    "74228458601-9b0apu4aplllg2cdkdbave9ogshhvkfs.apps.googleusercontent.com",
-  googleClientSecret: "6qjTsiWQSSSmo1FzUohEpMjy",
+    "395923047826-k7icbpfajp5d9a8cuogc8t6seqsq0vd4.apps.googleusercontent.com",
+  googleClientSecret: "jR2HxXQqJRSX9wO5JsArmVz0",
   mongoURI:
     "mongodb+srv://storybook:Paki$tan1@storybook-cluster-2ltka.mongodb.net/test?retryWrites=true"
 };
