f77a2b4e059523f5c0b65e352591adcf490c6349
unknown <hasnain shah>
2019-06-27 10:30:10 -0700

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
 
 const app = express();
 
-// loading router
-const authRouter = require("./routes/auth");
-
-// passport config
-require("./config/passport")(passport);
-// to the index page
 app.get("/", (req, res) => {
-  res.send("It is working");
+  res.send("Index");
 });
 
-//loading passport-google
-app.use("/auth", authRouter);
-// using router
-app.use("/auth", authRouter);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`app is running on ${port}`);
 });
